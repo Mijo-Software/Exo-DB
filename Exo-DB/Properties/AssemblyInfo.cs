51a2e07a689336fa25312c0bbb03709968a02ff9
Michael Johne <mjohne@users.noreply.github.com>
2019-06-06 19:20:45 +0200

    Add files via upload

---
+++ Exo-DB/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("EXO-DB3")]
+[assembly: AssemblyDescription("Verwaltungsprogramm für entdeckte Exoplaneten")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mijo Software")]
+[assembly: AssemblyProduct("EXO-DB")]
+[assembly: AssemblyCopyright("Copyright © 1995-2019")]
+[assembly: AssemblyTrademark("EXO-DB")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("dfc8fb8c-20d9-4b25-a216-6f10f6d96cb8")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// indem Sie "*" wie unten gezeigt eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
